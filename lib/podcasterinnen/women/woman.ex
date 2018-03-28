b20eb26a9abfb455f6590421168126c4341e19be
Michaela Lehr <michaela.lehr@geildanke.com>
2018-03-28 19:33:40 +0200

    MOD format podcasts in templates as list, add website to form

--- lib/podcasterinnen/women/woman.ex
+++ lib/podcasterinnen/women/woman.ex
@@ -7,6 +7,7 @@ defmodule Podcasterinnen.Women.Woman do
     field :forename, :string
     field :surname, :string
     field :twitter, :string
+    field :website, :string
     field :podcasts, {:array, :string}
 
     timestamps()
@@ -15,7 +16,7 @@ defmodule Podcasterinnen.Women.Woman do
   @doc false
   def changeset(woman, attrs) do
     woman
-    |> cast(attrs, [:forename, :surname, :twitter, :podcasts])
-    |> validate_required([:forename, :surname, :twitter, :podcasts])
+    |> cast(attrs, [:forename, :surname, :twitter, :website, :podcasts])
+    |> validate_required([:forename, :surname, :twitter, :website, :podcasts])
   end
 end
